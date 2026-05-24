9810d508631093d3607b052c7b1c574786563f29
BOCO SALOMON <boco0075620@univmetiers.ci>
2026-05-24 23:44:06 +0000

    Ajout de getters et setters

--- src/CompteBancaire.java
+++ src/CompteBancaire.java
@@ -1,12 +1,34 @@
 public class CompteBancaire {
 
-    public String compte;
+    private String compte;
+    private double solde;
 
-    public double solde;
-
+    // Constructeur
     public CompteBancaire(String compte, double solde) {
         this.compte = compte;
         this.solde = solde;
+    }
+
+    // SETTER : guichet pour modifier le compte
+    public void setCompte(String compte) {
+        this.compte = compte;
+    }
+
+    // GETTER : caissier pour lire le compte
+    public String getCompte() {
+        return this.compte;
+    }
+
+    // SETTER : guichet pour modifier le solde
+    public void setSolde(double solde) {
+        if (solde >= 0) {
+            this.solde = solde;
+        }
+    }
+
+    // GETTER : caissier pour lire le solde
+    public double getSolde() {
+        return this.solde;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,31 @@ public class Main {
         // Création d'un CompteCourant
         CompteCourant cc = new CompteCourant("CC-001", 150000, 50000);
         System.out.println("=== Compte Courant ===");
-        System.out.println("Compte : " + cc.compte);
-        System.out.println("Solde  : " + cc.solde);
+        System.out.println("Compte : " + cc.getCompte());
+        System.out.println("Solde  : " + cc.getSolde());
         System.out.println("Limite : " + cc.limite);
+
+        // Modification via setter
+        cc.setCompte("CC-999");
+        cc.setSolde(200000);
+        System.out.println("Après modification :");
+        System.out.println("Compte : " + cc.getCompte());
+        System.out.println("Solde  : " + cc.getSolde());
 
         // Création d'un CompteEpargne
         CompteEpargne ce = new CompteEpargne("CE-001", 500000, 3.5);
-        System.out.println("=== Compte Epargne ===");
-        System.out.println("Compte       : " + ce.compte);
-        System.out.println("Solde        : " + ce.solde);
+        System.out.println("\n=== Compte Epargne ===");
+        System.out.println("Compte       : " + ce.getCompte());
+        System.out.println("Solde        : " + ce.getSolde());
         System.out.println("Taux interet : " + ce.tauxInteret);
 
         // Création d'un CertificatDepot
         CertificatDepot cd = new CertificatDepot("CD-001", 1000000, 12);
-        System.out.println("=== Certificat de Depot ===");
-        System.out.println("Compte : " + cd.compte);
-        System.out.println("Solde  : " + cd.solde);
+        System.out.println("\n=== Certificat de Depot ===");
+        System.out.println("Compte : " + cd.getCompte());
+        System.out.println("Solde  : " + cd.getSolde());
         System.out.println("Duree  : " + cd.duree + " mois");
-
     }
+
 
 }
